1c15f56c2536d2dcc148db0a99037baca3dbfb61
foochane <fuchenkm@qq.com>
2018-05-24 21:28:57 +0800

    店铺列表展示之Contoller层的实现

--- src/main/java/com/chane/web/shopadmin/ShopManagementController.java
+++ src/main/java/com/chane/web/shopadmin/ShopManagementController.java
@@ -47,6 +47,54 @@ public class ShopManagementController {
     @Autowired
     private ShopService shopService;
 
+    @RequestMapping(value = "/getshopmanagementinfo", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getShopManagementInfo(HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<>();
+        long shopId = HttpServletRequestUtil.getLong(request,"shopId");
+        if(shopId <=0){
+            Object currentShopObj = request.getSession().getAttribute("currentShop");
+            if(currentShopObj == null){
+                modelMap.put("redirect",true);
+                modelMap.put("url","shop/shoplist");
+            }else {
+                Shop currentShop = (Shop)currentShopObj;
+                modelMap.put("redirect",false);
+                modelMap.put("shopId",currentShop.getShopId());
+            }
+        }else {
+            Shop currentShop = new Shop();
+            currentShop.setShopId(shopId);
+            request.getSession().setAttribute("currentShop",currentShop);
+            modelMap.put("redirect",false);
+        }
+
+        return modelMap;
+    }
+
+    @RequestMapping(value = "/getshoplist", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getShopList(HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<>();
+        PersonInfo user = new PersonInfo();
+        user.setUserId(1L);
+        request.getSession().setAttribute("user",user);
+        user = (PersonInfo)request.getSession().getAttribute("user");
+        try{
+            Shop shopCondition = new Shop();
+            shopCondition.setOwner(user);
+            ShopExecution se = shopService.getShopList(shopCondition,0,100);
+            modelMap.put("shopList",se.getShopList());
+            modelMap.put("user",user);
+            modelMap.put("success",true);
+        }catch (Exception e){
+            modelMap.put("success", false);
+            modelMap.put("errMsg", e.getMessage());
+        }
+
+        return  modelMap;
+    }
+
     @RequestMapping(value = "/modifyshop", method = RequestMethod.POST)
     @ResponseBody
     public Map<String, Object> modifyShop(HttpServletRequest request) {
